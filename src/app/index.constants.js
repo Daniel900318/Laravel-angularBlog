d92d3d660ccc979bb79589d1d3c6072c373c4772
Tsogtbayar Davaadorj <tsogtbayar0821@hotmail.com>
2016-03-15 13:14:58 +0200

    first

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('uglymona')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
